f0011f3938f0010cedb72c0fd77a0dcdb379c67a
Szymon Adach <32434997+dachu21@users.noreply.github.com>
2018-07-17 22:25:14 +0200

    Add Spring Boot project with basic configuration

---
+++ src/main/java/com/adach/scrumote/ScrumoteApplication.java
@@ -0,0 +1,12 @@
+package com.adach.scrumote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumoteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScrumoteApplication.class, args);
+  }
+}
