afd754806e7f721d2e77eeeef8c02ff4a0a6e6b0
Szymon Adach <32434997+dachu21@users.noreply.github.com>
2018-09-12 21:14:41 +0200

    Enable cookie-based CSRF protection

--- src/main/java/com/adach/scrumote/configuration/security/SecurityConfig.java
+++ src/main/java/com/adach/scrumote/configuration/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -39,8 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
-    //TODO enable csrf
-    http
-        .csrf().disable();
+    http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
   }
 }
