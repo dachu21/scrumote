dd187dced1f0a69b0264f18d1cd8854e6a4a4655
Szymon Adach <32434997+dachu21@users.noreply.github.com>
2018-09-21 23:08:21 +0200

    Enable CORS for localhost:4200 and add rest controller base path

--- scrumote-backend/src/main/java/com/adach/scrumote/api/UserController.java
+++ scrumote-backend/src/main/java/com/adach/scrumote/api/UserController.java
@@ -1,5 +1,6 @@
 package com.adach.scrumote.api;
 
+import com.adach.scrumote.configuration.mvc.PrefixedRestController;
 import com.adach.scrumote.dto.UserDto;
 import com.adach.scrumote.mapper.UserMapper;
 import com.adach.scrumote.service.UserService;
@@ -10,9 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@PrefixedRestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
 
