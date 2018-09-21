2406b8620b276f5ba260f0c04a515ea9aaef5b88
Szymon Adach <32434997+dachu21@users.noreply.github.com>
2018-09-22 00:14:38 +0200

    Refactor backend and frontend applications names

--- scrumote-backend/src/main/java/com/adach/scrumote/ScrumoteBackend.java
+++ scrumote-backend/src/main/java/com/adach/scrumote/ScrumoteBackend.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScrumoteApplication {
+public class ScrumoteBackend {
 
   public static void main(String[] args) {
-    SpringApplication.run(ScrumoteApplication.class, args);
+    SpringApplication.run(ScrumoteBackend.class, args);
   }
 }
