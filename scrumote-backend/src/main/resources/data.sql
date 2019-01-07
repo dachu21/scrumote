07ebe5cb950027fef04c0523d7bebcd0b29e5176
Szymon Adach <32434997+dachu21@users.noreply.github.com>
2019-01-07 09:43:20 +0100

    data sql

--- scrumote-backend/src/main/resources/data.sql
+++ scrumote-backend/src/main/resources/data.sql
@@ -1,3 +1,7 @@
+insert into system_feature_t (id, version, code, enabled) values
+(1, 0, 'REGISTRATION', true);
+alter sequence system_feature_seq restart with 2;
+
 insert into role_t (id, version, name) values
 (1, 0, 'ADMINISTRATOR'),
 (2, 0, 'SCRUM_MASTER'),
@@ -85,65 +89,19 @@ insert into role_permissions_t (role_id, permission_id) values
 (4, 32), (4, 33),
 (4, 38), (4, 39), (4, 40);
 
-insert into system_feature_t (id, version, code, enabled) values
-(1, 1, 'REGISTRATION', true);
-alter sequence system_feature_seq restart with 2;
-
 insert into user_t (id, version, active, password, username, email, first_name, last_name) values
-(1, 0, true, '$2a$10$4dA/YnrD2OhqYcN3KrnrZegKS7eJQRnwIkXhIkkOCBXCwNOnQ54vq', 'admin', 'a@server.com', 'A', 'Xyz'),
-(2, 0, true, '$2a$10$4dA/YnrD2OhqYcN3KrnrZegKS7eJQRnwIkXhIkkOCBXCwNOnQ54vq', 'master', 'm@server.com', 'M', 'Xyz'),
-(3, 0, true, '$2a$10$4dA/YnrD2OhqYcN3KrnrZegKS7eJQRnwIkXhIkkOCBXCwNOnQ54vq', 'owner', 'o@server.com', 'O', 'Xyz'),
-(4, 0, true, '$2a$10$4dA/YnrD2OhqYcN3KrnrZegKS7eJQRnwIkXhIkkOCBXCwNOnQ54vq', 'developer', 'd@server.com', 'D', 'Xyz'),
-(5, 0, true, '$2a$10$4dA/YnrD2OhqYcN3KrnrZegKS7eJQRnwIkXhIkkOCBXCwNOnQ54vq', 'amod', 'amod@server.com', 'AMOD', 'Xyz');
-alter sequence user_seq restart with 6;
+(1, 0, true, '$2a$10$4dA/YnrD2OhqYcN3KrnrZegKS7eJQRnwIkXhIkkOCBXCwNOnQ54vq', 'swagger', 'swagger@server.com', 'John', 'Doe');
+alter sequence user_seq restart with 2;
 
-insert into user_stats_t(id, version, average_first_vote_level_difference, first_votes_above_estimate, first_votes_below_estimate, first_votes_equal_estimate, issues, plannings, votes, user_id) values
-(1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-(2, 0, 0, 0, 0, 0, 0, 0, 0, 2),
-(3, 0, 0, 0, 0, 0, 0, 0, 0, 3),
-(4, 0, 0, 0, 0, 0, 0, 0, 0, 4),
-(5, 0, 0, 0, 0, 0, 0, 0, 0, 5);
-alter sequence user_stats_seq restart with 6;
+insert into user_stats_t(id, version, user_id, average_first_vote_level_difference, first_votes_above_estimate, first_votes_below_estimate, first_votes_equal_estimate, issues, plannings, votes) values
+(1, 0, 1, 0, 0, 0, 0, 0, 0, 0);
+alter sequence user_stats_seq restart with 2;
 
 insert into user_roles_t (user_id, role_id) values
 (1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 1),
-(5, 2),
-(5, 3),
-(5, 4);
+(1, 2),
+(1, 3),
+(1, 4);
 
 insert into user_permissions_t (user_id, permission_id) values
-(1, 1),
-(2, 1),
-(3, 1),
-(4, 1),
-(5, 1);
-
-insert into deck_t (id, version, name) values
-(1, 0, 'DECK_1');
-alter sequence deck_seq restart with 2;
-
-insert into card_t (id, version, level, "value", deck_id) values
-(1, 0, 1, '1', 1),
-(2, 0, 2, '2', 1),
-(3, 0, 3, '3', 1),
-(4, 0, 4, '5', 1),
-(5, 0, 5, '8', 1),
-(6, 0, 6, '13', 1),
-(7, 0, 7, '21', 1);
-alter sequence card_seq restart with 8;
-
-insert into planning_t (id, version, code, description, finished, name, deck_id, moderator_id) values
-(1, 0, 'PLANNING_1', 'Planning description 1', false, 'Planning 1', 1, 5);
-alter sequence planning_seq restart with 2;
-
-insert into planning_users_t (planning_id, user_id) values
-(1, 4),
-(1, 5);
-
-insert into issue_t (id, version, active, code, description, estimate, finished_iterations, name, planning_id) values
-(1, 0, false, 'ISSUE_1', 'Issue description 1', null, 0, 'Issue 1', 1);
-alter sequence issue_seq restart with 2;
+(1, 1);
